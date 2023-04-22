668f594cc6918d8a26f07e9c75e2b21e39912cd3
Jorge Mauricio <web@jorgemauricio.com>
2023-04-22 14:23:31 -0300

    Login blade and layout admin main blade config application file refactor.

--- syncsystem-laravel8-v1/resources/app_views/admin/layout-admin-main.blade.php
+++ syncsystem-laravel8-v1/resources/app_views/admin/layout-admin-main.blade.php
@@ -7,18 +7,18 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">{{-- Bootstrap required. --}}
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
         <title>@yield('cphTitle'){{-- $templateData['cphTitle'] --}}</title>
 
         <?php // Style Sheets - personalized. http://localhost:3000 | ../../ ?>
         <!--% if(gSystemConfig.configDebug === true){ %-->
-            <link rel="stylesheet" type="text/css" href="{{ asset('css/styles-backend.bundle.css') }}" media="screen" title="Default" /><!--Dev-->
+            <link rel="stylesheet" type="text/css" href="{{ asset('css/styles-backend.bundle.css') }}" media="screen" title="Default" />{{-- Dev --}}
         <!--% } %>
         <% if(gSystemConfig.configDebug === false){ %-->
             <!--link rel="stylesheet" type="text/css" href="/<%- gSystemConfig.configDirectoryDistSD %>/styles-backend.bundle.css" media="screen" title="Default" /--><!--Production (custom styles)-->
             <!--link rel="stylesheet" type="text/css" href="/<%- gSystemConfig.configDirectoryDistSD %>/styles-backend-vendor.bundle.css" media="screen" title="Default" /--><!--Production (vendor styles)-->
         <!--% } %-->
-        <link rel="canonical" href="<?php echo $GLOBALS['configSystemURL']; ?>" />
+        <link rel="canonical" href="<?php echo config('app.gSystemConfig.configSystemURL'); ?>" />
 
         {{--
             Favicon - 16x16 | 32x32 | 64x64 (pixels).
@@ -60,9 +60,9 @@
             unavailable_after: [RFC-850 date/time] - Don´t show page in index after a specif date.
         --}}
 
-        <meta name="author" content="<?php echo \SyncSystemNS\FunctionsGeneric::contentMaskRead($GLOBALS['configSystemClientName'], 'config-application'); ?>" />
-		<meta name="designer" content="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutDevName'); ?>" />
-		<meta name="copyright" content="<?php echo $GLOBALS['configCopyrightYear']; ?>, <?php echo \SyncSystemNS\FunctionsGeneric::contentMaskRead($GLOBALS['configSystemClientName'], 'config-application'); ?>" />
+        <meta name="author" content="<?php echo \SyncSystemNS\FunctionsGeneric::contentMaskRead(config('app.gSystemConfig.configSystemClientName'), 'config-application'); ?>" />
+		<meta name="designer" content="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutDevName'); ?>" />
+		<meta name="copyright" content="<?php echo config('app.gSystemConfig.configCopyrightYear'); ?>, <?php echo \SyncSystemNS\FunctionsGeneric::contentMaskRead(config('app.gSystemConfig.configSystemClientName'), 'config-application'); ?>" />
 		<meta name="rating" content="general" />{{-- general | mature | restricted | 14 years --}}
 
         {{-- JS includes. --}}
@@ -84,74 +84,72 @@
 
                 </div>
                 <div class="ss-backend-title-main" style="position: relative; display: block; height: 40px; line-height: 40px; text-indent: 15px; background-color: #d1d8dc; border: 1px solid #6c7880; border-radius: 0px 0px 15px 15px; margin-top: 2px; overflow: hidden;">
-                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutSystemName'); ?>
+                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutSystemName'); ?>
                 </div>
-                <div style="position: relative; display: block; height: 72px; background-image: url({{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-header-tb02-02.jpg') }}); background-repeat: repeat-x; background-position: top center; overflow: hidden;">
-                    <img src="{{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-header-tb02-03.jpg') }}" alt="Header Element" style="position: absolute; display: block; top: 0px; right: -8px;" /> 
-                    <img src="{{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-header-tb02-01.jpg') }}" alt="Header Element" style="position: absolute; display: block; top: 0px; left: -8px;" /> 
+                <div style="position: relative; display: block; height: 72px; background-image: url({{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-header-tb02-02.jpg') }}); background-repeat: repeat-x; background-position: top center; overflow: hidden;">
+                    <img src="{{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-header-tb02-03.jpg') }}" alt="Header Element" style="position: absolute; display: block; top: 0px; right: -8px;" />
+                    <img src="{{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-header-tb02-01.jpg') }}" alt="Header Element" style="position: absolute; display: block; top: 0px; left: -8px;" />
 
                     <aside style="position: absolute; display: block; top: 6px; right: 8px; width: 150px;">
                         <ul class="ss-backend-menu-ul01" style="color: #000000; list-style-type: disc; line-height: 11px;">
                             <li class="ss-backend-menu-li01">
-                                <a href="" 
-                                    class="ss-backend-menu-header-link" 
-                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuHome'); ?>">
-                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuHome'); ?>
+                                <a href=""
+                                    class="ss-backend-menu-header-link"
+                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuHome'); ?>">
+                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuHome'); ?>
                                 </a>
                             </li>
                             <li class="ss-backend-menu-li01">
-                                <a href="http://www.syncsystem.com.br" 
-                                    class="ss-backend-menu-header-link" 
-                                    target="_blank" 
-                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuSiteSystem'); ?>">
-                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuSiteSystem'); ?>
+                                <a href="http://www.syncsystem.com.br"
+                                    class="ss-backend-menu-header-link"
+                                    target="_blank"
+                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuSiteSystem'); ?>">
+                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuSiteSystem'); ?>
                                 </a>
                             </li>
                             <li class="ss-backend-menu-li01">
-                                <a href="http://www.syncsystem.com.br/pt/Contato.aspx" 
-                                    class="ss-backend-menu-header-link" 
-                                    target="_blank" 
-                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuContact'); ?>">
-                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuContact'); ?>
+                                <a href="http://www.syncsystem.com.br/pt/Contato.aspx"
+                                    class="ss-backend-menu-header-link"
+                                    target="_blank"
+                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuContact'); ?>">
+                                    <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuContact'); ?>
                                 </a>
                             </li>
                         </ul>
                     </aside>
                 </div>
-            </header>                    
+            </header>
 
             <main style="position: relative; display: flex; flex: 1; align-items: stretch;">
                 <?php // Left column. ?>
-                <div style="position: relative; display: block; min-width: 222px; max-width: 222px; background-image: url({{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-nav01-tb01-02.jpg') }}); background-position: left; background-repeat: repeat-y; overflow: hidden;">
-                    <img src="{{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-nav01-tb01-01.jpg') }}" alt="Nav Element" style="position: absolute; display: block; top: 0px; left: 0px; margin-top: -25px;" /> 
-                    
+                <div style="position: relative; display: block; min-width: 222px; max-width: 222px; background-image: url({{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-nav01-tb01-02.jpg') }}); background-position: left; background-repeat: repeat-y; overflow: hidden;">
+                    <img src="{{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-nav01-tb01-01.jpg') }}" alt="Nav Element" style="position: absolute; display: block; top: 0px; left: 0px; margin-top: -25px;" />
+
                     <nav style="position: relative; display: block; padding-top: 20px; margin-left: 37px; z-index: 1;">
                         <ul class="ss-backend-menu-ul01">
                             <li class="ss-backend-menu-li01">
-                                <a href="{{ '/' . $GLOBALS['configRouteBackend'] . '/' . $GLOBALS['configRouteBackendCategories'] . '/0' }}" 
-                                    class="ss-backend-menu-link" 
-                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuStartToolTip'); ?>">
-                                        <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuStart'); ?>
+                                <a href="{{ '/' . config('app.gSystemConfig.configRouteBackend') . '/' . config('app.gSystemConfig.configRouteBackendCategories') . '/0' }}"
+                                    class="ss-backend-menu-link"
+                                    title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuStartToolTip'); ?>">
+                                        <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuStart'); ?>
                                 </a>
                             </li>
-
-
 
                             {{-- <%# User - Root.%> --}}
                             {{-- <% if(additionalData.cookiesData[gSystemConfig.configCookiePrefix + "_" + gSystemConfig.configCookiePrefixUserRoot]){ %> --}}
                                 <li class="ss-backend-menu-li01">
-                                    <a href="{{ '/' . $GLOBALS['configRouteBackend'] . '/' . $GLOBALS['configRouteBackendUsers'] . '/' . $GLOBALS['configRouteBackendActionEdit'] . '/123' /* TODO: substitute with the id */ }}" 
-                                        class="ss-backend-menu-link" 
-                                        title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuUsersPasswordEditToolTip'); ?>">
-                                            <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuUsersPasswordEdit'); ?>
+                                    <a href="{{ '/' . config('app.gSystemConfig.configRouteBackend') . '/' . config('app.gSystemConfig.configRouteBackendUsers') . '/' . config('app.gSystemConfig.configRouteBackendActionEdit') . '/123' /* TODO: substitute with the id */ }}"
+                                        class="ss-backend-menu-link"
+                                        title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuUsersPasswordEditToolTip'); ?>">
+                                            <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuUsersPasswordEdit'); ?>
                                     </a>
                                 </li>
 
                                 <li class="ss-backend-menu-li01">
-                                    <a href="{{ '/' . $GLOBALS['configRouteBackend'] . '/' . $GLOBALS['configRouteBackendLogOff'] . '/' }}" 
-                                        class="ss-backend-menu-link" 
-                                        title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuLogUsersOffToolTip'); ?>">
-                                            <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendMenuUsersLogOff'); ?>
+                                    <a href="{{ '/' . config('app.gSystemConfig.configRouteBackend') . '/' . config('app.gSystemConfig.configRouteBackendLogOff') . '/' }}"
+                                        class="ss-backend-menu-link"
+                                        title="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuLogUsersOffToolTip'); ?>">
+                                            <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendMenuUsersLogOff'); ?>
                                     </a>
                                 </li>
                             {{-- <% } %> --}}
@@ -159,8 +157,8 @@
                     </nav>
 
                     <footer class="ss-backend-copyright" style="position: absolute; display: block; bottom: 0px; left: 0px; background-color: #ffffff; padding-bottom: 8px;">
-                        <img src="{{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-nav01-tb01-03.jpg') }}" alt="Footer Element" style="position: absolute; display: block; top: -56px; left: 0px;" /> 
-                        <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutCopyright') . ' ' . $GLOBALS['configCopyrightYear'] . ' ® ' . \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutDevName'); ?> 
+                        <img src="{{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-nav01-tb01-03.jpg') }}" alt="Footer Element" style="position: absolute; display: block; top: -56px; left: 0px;" />
+                        <?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutCopyright') . ' ' . config('app.gSystemConfig.configCopyrightYear') . ' ® ' . \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutDevName'); ?>
                     </footer>
                 </div>
                 <?php // Left column. ?>
@@ -190,7 +188,7 @@
                     <!--%-templateData.cphBody %-->
                     {{--
                     cphBody = {{ $templateData['cphBody'] }} <br />
-                    additionalData = 
+                    additionalData =
                     @php
                         var_dump($templateData['additionalData']);
                     @endphp
@@ -206,7 +204,7 @@
         <?php // Ajax progress bar. ?>
         <div id="updtProgressGeneric" class="ss-backend-progress-bar-generic1-container" style="display: none;">
             <div class="ss-backend-progress-bar-generic1">
-                <img src="{{ asset('/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/backend-layout-progress-bar-generic1.gif') }}" alt="" />
+                <img src="{{ asset('/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/backend-layout-progress-bar-generic1.gif') }}" alt="" />
             </div>
         </div>
 
--- syncsystem-laravel8-v1/resources/app_views/admin/login.blade.php
+++ syncsystem-laravel8-v1/resources/app_views/admin/login.blade.php
@@ -16,8 +16,8 @@
     $metaKeywords = '';
 
     // Meta URL current.
-    $metaURLCurrent = $GLOBALS['configSystemURL'] . '/';
-    $metaURLCurrent .= $GLOBALS['configRouteBackend'] . '/';
+    $metaURLCurrent = config('app.gSystemConfig.configSystemURL') . '/';
+    $metaURLCurrent .= config('app.gSystemConfig.configRouteBackend') . '/';
     if ($masterPageSelect !== '') {
         $metaURLCurrent .= '?masterPageSelect=' . $masterPageSelect;
     }
@@ -33,18 +33,18 @@
 @endsection
 
 @section('cphHead')
-    <meta name="title" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaTitle); ?>" /> {{-- Bellow 160 characters. --}}
-    <meta name="description" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaDescription); ?>" /> {{-- Bellow 100 characters. --}}
-    <meta name="keywords" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaKeywords); ?>" /> {{-- Bellow 60 characters. --}}
+    <meta name="title" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaTitle); ?>" />{{-- Bellow 160 characters. --}}
+    <meta name="description" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaDescription); ?>" />{{-- Bellow 100 characters. --}}
+    <meta name="keywords" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaKeywords); ?>" />{{-- Bellow 60 characters. --}}
 
     {{-- Open Graph tags. --}}
     <meta property="og:title" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaTitle); ?>" />
-    <meta property="og:type" content="website" /> {{-- http:// ogp.me/#types | https:// developers.facebook.com/docs/reference/opengraph/ --}}
+    <meta property="og:type" content="website" />{{-- http:// ogp.me/#types | https:// developers.facebook.com/docs/reference/opengraph/ --}}
     <meta property="og:url" content="<?php echo $metaURLCurrent; ?>" />
     <meta property="og:description" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaDescription); ?>" />
-    <meta property="og:image" content="<?php echo $GLOBALS['configSystemURL'] . '/' . $GLOBALS['configDirectoryFilesLayoutSD'] . '/' . 'icon-logo-og.png'; ?>" /> {{-- The recommended resolution for the OG image is 1200x627 pixels, the size up to 5MB. // 120x120px, up to 1MB JPG ou PNG, lower than 300k and minimum dimension 300x200 pixels. --}}
+    <meta property="og:image" content="<?php echo config('app.gSystemConfig.configSystemURL') . '/' . config('app.gSystemConfig.configDirectoryFilesLayoutSD') . '/' . 'icon-logo-og.png'; ?>" />{{-- The recommended resolution for the OG image is 1200x627 pixels, the size up to 5MB. // 120x120px, up to 1MB JPG ou PNG, lower than 300k and minimum dimension 300x200 pixels. --}}
     <meta property="og:image:alt" content="<?php echo \SyncSystemNS\FunctionsGeneric::removeHTML01($metaTitle); ?>" />
-    <meta property="og:locale" content="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'configBackendLanguage'); ?>" />
+    <meta property="og:locale" content="<?php echo \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'configBackendLanguage'); ?>" />
 @endsection
 
 @section('cphTitleCurrent')
@@ -53,64 +53,69 @@
 
 @section('cphBody')
     {{-- Desktop version. --}}
-    <div style="position: absolute; 
-        display: block; 
-        height: 270px; 
-        width: 100%; 
-        top: 50%; 
-        margin-top: -135px; 
-        background-image: url(/{{ $GLOBALS['configDirectoryFilesLayoutSD'] }}/backend-layout-login-bg01.jpg), url(/{{ $GLOBALS['configDirectoryFilesLayoutSD'] }}/backend-layout-login-bg02.jpg);
+    <div style="position: absolute;
+        display: block;
+        height: 270px;
+        width: 100%;
+        top: 50%;
+        margin-top: -135px;
+        background-image: url(/{{ config('app.gSystemConfig.configDirectoryFilesLayoutSD') }}/backend-layout-login-bg01.jpg), url(/{{ config('app.gSystemConfig.configDirectoryFilesLayoutSD') }}/backend-layout-login-bg02.jpg);
         background-position: top, bottom;
-        background-repeat: repeat-x, repeat-x;">
-            {{-- Logo. --}}
-        <img src="/{{ $GLOBALS['configDirectoryFilesLayoutSD'] }}/backend-layout-logo-client.png" 
-            alt="Logo - {{ \SyncSystemNS\FunctionsGeneric::contentMaskRead($GLOBALS['configSystemClientName'], 'config-application') }}" 
+        background-repeat: repeat-x, repeat-x;"
+    >
+        {{-- Logo. --}}
+        <img src="/{{ config('app.gSystemConfig.configDirectoryFilesLayoutSD') }}/backend-layout-logo-client.png"
+            alt="Logo - {{ \SyncSystemNS\FunctionsGeneric::contentMaskRead(config('app.gSystemConfig.configSystemClientName'), 'config-application') }}"
             style="max-height: 230px;
-            position: absolute;  
-            top: 0px;  
-            bottom: 0px;  
-            left: 10px;  
-            margin: auto;" />
+            position: absolute;
+            top: 0px;
+            bottom: 0px;
+            left: 10px;
+            margin: auto;"
+        />
 
         {{-- Header. --}}
         <header class="ss-backend-title01" style="position: absolute; display: block; right: 10px; top: 30px; text-align: right; font-size: 24px;">
-            {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutSystemName') }}
+            {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutSystemName') }}
         </header>
 
         {{-- Login. --}}
         <main class="ss-backend-text01" style="position: absolute; display: block; right: 10px; bottom: 30px; text-align: right;">
             @include('admin.partial-messages-status')
-            
-            <form id="formLogin" name="formLogin" method="POST" action="/{{ $GLOBALS['configRouteBackend'] . '/' . $GLOBALS['configRouteBackendLogin'] }}" enctype="multipart/form-data">
+
+            <form id="formLogin" name="formLogin" method="POST" action="/{{ config('app.gSystemConfig.configRouteBackend') . '/' . config('app.gSystemConfig.configRouteBackendLogin') }}" enctype="multipart/form-data">
                 @csrf
-                
+
                 <div style="position: relative; display: block;">
-                    {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendLoginUser') }}:
+                    {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendLoginUser') }}:
                     <input type="text" id="username" name="username" class="ss-backend-field-text01" />
                 </div>
                 <div style="position: relative; display: block; margin: 5px 0px;">
-                    {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendLoginPassword') }}:
+                    {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendLoginPassword') }}:
                     <input type="password" id="password" name="password" class="ss-backend-field-text01" />
                 </div>
                 <div style="position: relative; display: block;">
                     <button class="ss-backend-btn-base ss-backend-btn-action">
-                        {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'backendButtonLogin') }}
+                        {{ \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'backendButtonLogin') }}
                     </button>
                 </div>
             </form>
         </main>
 
         {{-- Footer. --}}
-        <footer class="ss-backend-copyright" style="position: absolute; 
-                                                    display: block;
-                                                    right: 0px;
-                                                    bottom: -5px;  
-                                                    left: 0px; 
-                                                    text-align: center;">
-            {{ 
-                \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutCopyright') . ' ® ' . 
-                \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutDevName') . ' ' . 
-                \SyncSystemNS\FunctionsGeneric::appLabelsGet($GLOBALS['configLanguageBackend']->appLabels, 'layoutCopyright1')
+        <footer
+            class="ss-backend-copyright"
+            style="position: absolute;
+                    display: block;
+                    right: 0px;
+                    bottom: -5px;
+                    left: 0px;
+                    text-align: center;"
+        >
+            {{
+                \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutCopyright') . ' ® ' .
+                \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutDevName') . ' ' .
+                \SyncSystemNS\FunctionsGeneric::appLabelsGet(config('app.gSystemConfig.configLanguageBackend')->appLabels, 'layoutCopyright1')
             }}
         </footer>
     </div>
